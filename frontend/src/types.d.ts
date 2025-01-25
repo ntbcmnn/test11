5246f213e2194ebdd53228b16c2650e9b749f64e
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-25 18:35:37 +0600

    Created interfaces for Products and categories

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -38,3 +38,33 @@ export interface ValidationError {
 export interface GlobalError {
   error: string;
 }
+
+export interface IProduct {
+  _id: string;
+  user: {
+    username: string;
+    display_name: string;
+    phone_number: string;
+  };
+  category: {
+    name: string;
+  };
+  title: string;
+  description: string;
+  price: number;
+  image: File | null;
+}
+
+export interface ICategory {
+  _id: string;
+  name: string;
+}
+
+export interface IProductMutation {
+  category: ICategory;
+  title: string;
+  description: string;
+  price: string;
+  image: File | null;
+}
+
