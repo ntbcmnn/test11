8fd124091fe679f0cf4ad188796c5297a008b368
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-25 18:34:16 +0600

    Updated toolbar by adding a list of categories to navigate user

--- frontend/src/Components/UI/Toolbar/Toolbar.tsx
+++ frontend/src/Components/UI/Toolbar/Toolbar.tsx
@@ -1,11 +1,20 @@
 import { NavLink } from 'react-router-dom';
-import { useAppSelector } from '../../../app/hooks.ts';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks.ts';
 import { selectUser } from '../../../store/slices/usersSlice.ts';
 import UserMenu from './UserMenu.tsx';
 import AnonymousMenu from './AnonymousMenu.tsx';
+import { selectCategories } from '../../../store/slices/categoriesSlice.ts';
+import { useEffect } from 'react';
+import { getCategories } from '../../../store/thunks/categoriesThunk.ts';
 
 const Toolbar = () => {
   const user = useAppSelector(selectUser);
+  const categories = useAppSelector(selectCategories);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
 
   return (
     <nav className="navbar navbar-expand-lg bg-dark">
@@ -16,6 +25,14 @@ const Toolbar = () => {
         >
           Marketplace
         </NavLink>
+
+        <div className="d-flex align-items-center justify-content-between gap-3">
+          {categories.map((category) => (
+            <NavLink key={category._id} to={`products/category/${category._id}`}
+                     className="text-decoration-none menu-item text-white">{category.name}</NavLink>
+          ))}
+        </div>
+
         <div className="d-flex justify-content-end gap-3">
           {user ? <UserMenu user={user}/> : <AnonymousMenu/>}
         </div>
