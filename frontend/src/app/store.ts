ee006944609a9ab6e14be143748f6e4d97b5ff78
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-25 18:36:33 +0600

    Imported categories and products reducers

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,25 +1,20 @@
-import { usersReducer } from "../store/slices/usersSlice.ts";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  persistStore,
-  persistReducer,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-} from "redux-persist";
+import { usersReducer } from '../store/slices/usersSlice.ts';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE, } from 'redux-persist';
+import { categoriesReducer } from '../store/slices/categoriesSlice.ts';
+import { productsReducer } from '../store/slices/productsSlice.ts';
 
 const usersPersistConfig = {
-  key: "store:users",
+  key: 'store:users',
   storage,
-  whitelist: ["user"],
+  whitelist: ['user'],
 };
 
 const rootReducer = combineReducers({
   users: persistReducer(usersPersistConfig, usersReducer),
+  categories: categoriesReducer,
+  products: productsReducer,
 });
 
 export const store = configureStore({
