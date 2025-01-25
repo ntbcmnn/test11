38adf956653859317852abbfad9c8f6fc356f935
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-25 14:21:06 +0600

    Updated UserFields interface and added interfaces for product and category

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,5 +1,22 @@
+import {Types} from 'mongoose';
+
 export interface UserFields {
     username: string;
     password: string;
     token: string;
+    display_name: string;
+    phone_number: string;
+}
+
+export interface IProduct {
+    seller: Types.ObjectId;
+    category: Types.ObjectId;
+    title: string;
+    description: string;
+    price: number;
+    image: string | null;
+}
+
+export interface ICategory {
+    name: string;
 }
