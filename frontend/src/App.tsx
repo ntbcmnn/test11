c10e1ef98232c160123a24100f22367344b2c585
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-25 18:40:24 +0600

    Added all required routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-import "./App.css";
-import Toolbar from "./Components/UI/Toolbar/Toolbar.tsx";
-import { Route, Routes } from "react-router-dom";
-import LoginPage from "./Containers/LoginPage/LoginPage.tsx";
-import RegisterPage from "./Containers/RegisterPage/RegisterPage.tsx";
+import './App.css';
+import Toolbar from './Components/UI/Toolbar/Toolbar.tsx';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './Containers/LoginPage/LoginPage.tsx';
+import RegisterPage from './Containers/RegisterPage/RegisterPage.tsx';
+import AddProduct from './Containers/AddProduct/AddProduct.tsx';
+import Products from './Containers/Products/Products.tsx';
+import DetailedProduct from './Containers/DetailedProduct/DetailedProduct.tsx';
 
 const App = () => {
   return (
@@ -10,8 +13,13 @@ const App = () => {
       <Toolbar />
       <div className="container my-5">
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<Products/>}/>
+          <Route path="/products" element={<Products/>}/>
+          <Route path="/products/:productId" element={<DetailedProduct/>}/>
+          <Route path="/products/category/:categoryId" element={<DetailedProduct/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="add-product" element={<AddProduct/>}/>
           <Route
             path="*"
             element={<h3 className="text-center">Page not found</h3>}
