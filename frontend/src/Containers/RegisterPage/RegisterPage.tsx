1bc67af5fb48831c7575e01306af2e2d8cb2aa6f
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-25 14:26:51 +0600

    Updated register form by adding new fields and validations for them, also added the button spinner component

--- frontend/src/Containers/RegisterPage/RegisterPage.tsx
+++ frontend/src/Containers/RegisterPage/RegisterPage.tsx
@@ -1,29 +1,33 @@
-import React, { useState } from "react";
-import { RegisterMutation } from "../../types";
-import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
-import { selectRegisterError } from "../../store/slices/usersSlice.ts";
-import { NavLink, useNavigate } from "react-router-dom";
-import { register } from "../../store/thunks/usersThunk.ts";
+import React, { useState } from 'react';
+import { RegisterMutation } from '../../types';
+import { useAppDispatch, useAppSelector } from '../../app/hooks.ts';
+import { selectRegisterError, selectRegisterLoading } from '../../store/slices/usersSlice.ts';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { register } from '../../store/thunks/usersThunk.ts';
+import ButtonLoading from '../../Components/UI/ButtonLoading/ButtonLoading.tsx';
 
 const RegisterPage = () => {
   const dispatch = useAppDispatch();
   const registerError = useAppSelector(selectRegisterError);
+  const isLoading = useAppSelector(selectRegisterLoading);
   const navigate = useNavigate();
   const [form, setForm] = useState<RegisterMutation>({
-    username: "",
-    password: "",
+    username: '',
+    password: '',
+    display_name: '',
+    phone_number: '',
   });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm((prevState) => ({ ...prevState, [name]: value }));
+    const {name, value} = e.target;
+    setForm((prevState) => ({...prevState, [name]: value}));
   };
 
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       await dispatch(register(form)).unwrap();
-      navigate("/");
+      navigate('/');
     } catch (e) {
       console.log(e);
     }
@@ -38,7 +42,7 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="container mt-5" style={{ maxWidth: "400px" }}>
+    <div className="container mt-5" style={{maxWidth: '400px'}}>
       <div className="text-center mb-4">
         <i className="bi bi-lock fs-2 "></i>
         <h2 className="mt-2">Sign Up</h2>
@@ -54,10 +58,10 @@ const RegisterPage = () => {
             name="username"
             value={form.username}
             onChange={onChange}
-            className={`form-control ${getFieldError("username") ? "is-invalid" : ""}`}
+            className={`form-control ${getFieldError('username') ? 'is-invalid' : ''}`}
           />
-          {getFieldError("username") && (
-            <div className="invalid-feedback">{getFieldError("username")}</div>
+          {getFieldError('username') && (
+            <div className="invalid-feedback">{getFieldError('username')}</div>
           )}
         </div>
 
@@ -71,16 +75,55 @@ const RegisterPage = () => {
             name="password"
             value={form.password}
             onChange={onChange}
-            className={`form-control ${getFieldError("password") ? "is-invalid" : ""}`}
+            className={`form-control ${getFieldError('password') ? 'is-invalid' : ''}`}
           />
-          {getFieldError("password") && (
-            <div className="invalid-feedback">{getFieldError("password")}</div>
+          {getFieldError('password') && (
+            <div className="invalid-feedback">{getFieldError('password')}</div>
           )}
         </div>
 
-        <button type="submit" className="btn btn-dark w-100">
-          Sign Up
-        </button>
+        <div className="mb-3">
+          <label htmlFor="display_name" className="form-label">
+            Seller's name
+          </label>
+          <input
+            type="text"
+            id="display_name"
+            name="display_name"
+            value={form.display_name}
+            onChange={onChange}
+            className={`form-control ${getFieldError('display_name') ? 'is-invalid' : ''}`}
+          />
+          {getFieldError('display_name') && (
+            <div className="invalid-feedback">{getFieldError('display_name')}</div>
+          )}
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="phone_number" className="form-label">
+            Phone number
+          </label>
+          <input
+            type="text"
+            id="phone_number"
+            name="phone_number"
+            value={form.phone_number}
+            onChange={onChange}
+            className={`form-control ${getFieldError('phone_number') ? 'is-invalid' : ''}`}
+          />
+          {getFieldError('phone_number') && (
+            <div className="invalid-feedback">{getFieldError('phone_number')}</div>
+          )}
+        </div>
+
+        <div className="mb-3">
+          <ButtonLoading
+            type="submit"
+            text="Sign up"
+            isLoading={isLoading}
+            isDisabled={isLoading}
+          />
+        </div>
 
         <div className="text-center mt-3">
           <NavLink to="/login" className="text-decoration-none">
