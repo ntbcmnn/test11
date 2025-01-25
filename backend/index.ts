401c486e70437d7e46eaef4fb63476a9718d6c08
Natalya Botsman <botsmannatashaa@gmail.com>
2025-01-25 14:20:16 +0600

    Imported all required routers for users, categories and products

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import usersRouter from './routers/users';
 import config from './config';
+import usersRouter from './routers/users';
+import categoriesRouter from './routers/categories';
+import productsRouter from './routers/products';
 
 const app = express();
 const port = 8000;
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/users', usersRouter);
+app.use('/categories', categoriesRouter);
+app.use('/products', productsRouter);
 
 const run: () => Promise<void> = async () => {
     await mongoose.connect(config.db);
